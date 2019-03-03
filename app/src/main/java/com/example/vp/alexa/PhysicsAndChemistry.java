515e2198ed466524393651f7bd6e1e8b97c64cd8
Arlecuno <palchinskij19@gmail.com>
2019-03-03 19:59:07 +0200

    Content added. All classes were created.

---
+++ app/src/main/java/com/example/vp/alexa/PhysicsAndChemistry.java
@@ -0,0 +1,13 @@
+package com.example.vp.alexa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhysicsAndChemistry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_physics_and_chemistry);
+    }
+}
---
+++ app/src/main/java/com/example/vp/alexa/TimesAndAlarm.java
@@ -0,0 +1,13 @@
+package com.example.vp.alexa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimesAndAlarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_times_and_alarm);
+    }
+}
